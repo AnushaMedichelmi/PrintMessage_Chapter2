4ff2524379027e3367b32e74115be54d961cfe9f
PTRL481\Anusha.Medichelmi <anusha.m@xcubelabs.com>
2022-05-06 10:52:29 +0530

    Printing a message in unity using c sharp

---
+++ Assets/PrintRainMessage.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintRainMessage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello World");
+        Debug.Log("I'm Anusha Medichelmi from RGUKT BASAR");
+        Debug.Log("Is it Raining in Hyderabad ?");
+        Debug.Log("Give the information regarding Hyderabad Weather");
+        Debug.Log("Thank You");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
